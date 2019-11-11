8ce00dd5c200274241adf3e86f538ba6ce4d2f53
andrewbastian <andrew.bastian@gmail.com>
2019-11-11 15:37:54 -0800

    Still short MVP, Issue with update and add - think it is a naming issue. Delet works

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,74 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Link } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import MovieForm from "./Movies/MovieForm";
+import UpdateMovie from './Movies/UpdateMovie'
+import axios from "axios";
 
 const App = () => {
+
   const [savedList, setSavedList] = useState([]);
 
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/movies")
+      .then(res => setMovies(res.data))
+      .catch(err => console.log(err.response));
+  }, [])
+
+  const updateMovies = (updatedMovie) => {
+  const newMovies = movies.map(movie => {
+    if (movie.id === updatedMovie.id) return updatedMovie;
+    else return movie
+  })
+  setMovies(newMovies);
+}
+
+const deleteMovie = id => {
+  axios.delete(`http://localhost:5000/api/movies/${id}`)
+  .then(res => {
+    setMovies(movies.filter(movie => movie.id !== res.data));
+    })
+    .catch(err => console.log(err))
+}
+
   const addToSavedList = movie => {
+
     setSavedList([...savedList, movie]);
+    savedList.push(movie)
+    setSavedList({savedList})
   };
 
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
-      <Route
-        path="/movies/:id"
-        render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+
+      <Link to="/add">Add New Movie</Link>
+
+        <Route
+            exact path="/"
+            render={props => <MovieList {...props} movies={movies} />}
+            />
+
+        <Route path="/add" component={MovieForm} />
+
+        <Route
+            path="/update-movie/:id"
+            render= {props => <UpdateMovie {...props} savedList={savedList} updateSavedList={setSavedList}/>}
+          />
+
+              <Route
+                path="/movies/:id"
+                render={props => {
+
+                  return <Movie {...props} addToSavedList={addToSavedList} updateSavedList={setSavedList}/>;
         }}
       />
+
     </>
   );
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink, Link } from 'react-router-dom';
+
 export default class SavedList extends Component {
   constructor(props) {
     super(props);
@@ -7,22 +8,39 @@ export default class SavedList extends Component {
 
   render() {
     return (
+
       <div className="saved-list">
+
         <h3>Saved Movies:</h3>
+        
         {this.props.list.map(movie => {
           return (
+
             <NavLink
               to={`/movies/${movie.id}`}
               key={movie.id}
               activeClassName="saved-active"
             >
+
               <span className="saved-movie">{movie.title}</span>
+
             </NavLink>
           );
         })}
-        <div className="home-button">
-          <Link to="/">Home</Link>
+
+        <div>
+
+          <button className="home-button">
+            <Link to="/">Home</Link>
+
+          </button>
+
+          <button className="home-button">
+            <Link to="/add-movie">Add Movie</Link>
+          </button>
+
         </div>
+
       </div>
     );
   }
